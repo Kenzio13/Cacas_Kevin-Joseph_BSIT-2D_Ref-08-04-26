c96611ad776fc05dd9aebd36b0b66f1928fb5ce0
Kevin Joseph Cacas <kevincacasc@gmail.com>
2026-08-04 23:27:53 +0800

    Add files via upload

---
+++ kevin_cacas.py
@@ -0,0 +1,6 @@
+name = ("Kevin Cacas")
+age = ("20")
+address = ("Valdez Floridablanca")
+favcolor = ("Black")
+
+print(f"Hi! My name is {name}. I am {age} years old and I live in {address}. My favorite color is {favcolor}.")
